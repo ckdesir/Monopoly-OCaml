412133b71c179b83d6ebff81a76937e6b440c4ea
Chris Desir <christopher.desir01@gmail.com>
2021-03-23 14:36:11 -0400

    Add basic intro files

---
+++ author.mli
@@ -0,0 +1,31 @@
+(** CS 3110 Spring 2021 Final Assignmment
+
+    @authors Christopher Desir (ckd38) 
+    
+*)
+
+(************************************************************ 
+
+   Academic Integrity Statement
+
+   We, Christopher Desir, the persons named in the @authors comment above, 
+   have fully reviewed the course academic integrity policies.  
+   We have adhered to those policies in solving the assignment.
+
+   The policies do permit some limited collaboration among students currently
+   enrolled in the course. If we did engage in such collaboration, here is the
+   list of other students with whom we collaborated, and a brief summary of that
+   collaboration:
+
+   - none
+
+   Here are any deviations from the policies that we want to document, with the
+   understanding that being honest about my mistakes is more honorable than
+   lying about my intellectual work:
+
+   - none
+
+ ************************************************************)
+
+(** [hours_worked] is the number of hours you worked on this assignment. *)
+val hours_worked : int
