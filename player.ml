0bf602e0ffec5a8b4c81ea4e1533b3dcaaf1eee8
jdkochian <71579866+jdkochian@users.noreply.github.com>
2021-03-28 14:48:41 -0400

    Implemetned player compilation unit

--- player.ml
+++ player.ml
@@ -1,30 +1,139 @@
 type player_id = string
 
+exception InsufficientFunds
+
 type player = {
   name : player_id;
   piece : string;
-  current_square : string;
-  mutable balance : int ref;
-  properties : string list;
+  current_square : Board.square_name;
+  mutable balance : int;
+  properties : Board.square_name list;
+  is_bankrupt : bool;
+  get_out_of_jail_cards : int;
 }
 
 type t = player
 
-let balance t = !(t.balance)
+let name t = t.name
 
 let piece t = t.piece
 
 let current_square t = t.current_square
 
+let balance t = t.balance
+
 let properties t = t.properties
+
+let bankrupt (player : player) : bool = player.is_bankrupt
 
 let is_in_jail t =
   match t.current_square with "jail" -> true | _ -> false
 
 let pay amt giver recip =
-  giver.balance := !(giver.balance) - amt;
-  recip.balance := !(recip.balance) + amt;
+  if amt > giver.balance then raise InsufficientFunds
+  else giver.balance <- giver.balance - amt;
+  recip.balance <- recip.balance + amt;
   ()
 
-let move (player : player) (amt : int) (curr : string) : player =
-  failwith "Unimplemented"
+let bank_transaction amt player =
+  let x = player.balance + amt in
+  if x < 0 then raise InsufficientFunds else player.balance <- x;
+  ()
+
+let pass_go player = bank_transaction 200 player
+
+let jail_cards (player : player) : int = player.get_out_of_jail_cards
+
+let send_to_jail (player : player) : player =
+  match jail_cards player with
+  | 0 ->
+      {
+        name = player.name;
+        piece = player.piece;
+        current_square = "jail";
+        balance = player.balance;
+        properties = player.properties;
+        is_bankrupt = player.is_bankrupt;
+        get_out_of_jail_cards = 0;
+      }
+  | x ->
+      {
+        name = player.name;
+        piece = player.piece;
+        current_square = player.current_square;
+        balance = player.balance;
+        properties = player.properties;
+        is_bankrupt = player.is_bankrupt;
+        get_out_of_jail_cards = x - 1;
+      }
+
+let owns player square = List.mem square player.properties
+
+let trade
+    p1
+    p2
+    (p1_props : Board.square_name list)
+    (p2_props : Board.square_name list) : player * player =
+  let remove_elts elt_lst lst =
+    List.filter (fun x -> not (List.mem x elt_lst)) lst
+  in
+
+  let remove_props player prop_lst =
+    let new_lst = remove_elts prop_lst player.properties in
+    {
+      name = player.name;
+      piece = player.piece;
+      current_square = player.current_square;
+      balance = player.balance;
+      properties = new_lst;
+      is_bankrupt = player.is_bankrupt;
+      get_out_of_jail_cards = player.get_out_of_jail_cards;
+    }
+  in
+
+  let get_props player prop_list =
+    let new_lst =
+      List.merge Stdlib.compare player.properties prop_list
+    in
+    {
+      name = player.name;
+      piece = player.piece;
+      current_square = player.current_square;
+      balance = player.balance;
+      properties = new_lst;
+      is_bankrupt = player.is_bankrupt;
+      get_out_of_jail_cards = player.get_out_of_jail_cards;
+    }
+  in
+
+  let p1 = remove_props p1 p1_props in
+  let p1 = get_props p1 p2_props in
+  let p2 = remove_props p2 p2_props in
+  let p2 = get_props p2 p1_props in
+
+  (p1, p2)
+
+let trade_cards giver recip amt =
+  let giver =
+    {
+      name = giver.name;
+      piece = giver.piece;
+      current_square = giver.current_square;
+      balance = giver.balance;
+      properties = giver.properties;
+      is_bankrupt = giver.is_bankrupt;
+      get_out_of_jail_cards = giver.get_out_of_jail_cards - amt;
+    }
+  in
+  let recip =
+    {
+      name = recip.name;
+      piece = recip.piece;
+      current_square = recip.current_square;
+      balance = recip.balance;
+      properties = recip.properties;
+      is_bankrupt = recip.is_bankrupt;
+      get_out_of_jail_cards = recip.get_out_of_jail_cards + amt;
+    }
+  in
+  (giver, recip)
--- player.mli
+++ player.mli
@@ -6,16 +6,67 @@ type t
 (** Type of player identifier *)
 type player_id = string
 
-(** Balance of a player *)
-val balance : t -> int ref
+(** Raised when a player tries to perform an action but has insufficient
+    balance*)
+exception InsufficientFunds
 
-(** TODO: change this type *)
-val current_square : t -> string
+(**[name t] is the string representing the name of the person playing as
+   the player [t]*)
+val name : t -> player_id
 
-(**TODO: check this type*)
+(** [piece t] is the token that the player [t] is represented by on a
+    physical monopoly board. For example, ["thimble"], ["top hat"] would
+    be valid values of [piece t] on a traditional monopoly board*)
 val piece : t -> string
 
-(**TODO: change this type*)
-val properties : t -> string list
+(** [current_square t] is the square that player [t] occupies. *)
+val current_square : t -> Board.square_name
 
+(** [balance t] is the value of the mutable balance of player [t]*)
+val balance : t -> int
+
+(**[properties t] is the list of properties that are owned by the player
+   represented by [t] *)
+val properties : t -> Board.square_name list
+
+(**[bankrupt t] represents whether or not the player [t] is bankrupt,
+   and thus determines if they are still in the game*)
+val bankrupt : t -> bool
+
+(** [is_in_jail t] is whether or not the player represented by [t] is
+    currently in jail. *)
+val is_in_jail : t -> bool
+
+(** [pay int t p] alters two player's mutable balances to perform the
+    physical action of paying in Monopoly. For example, if [t] had $600
+    and [p] had $400 then [pay 100 t p] would result in [balance t]
+    being [500] and [balance p] being [500]. Raises [InsufficentFunds]
+    if [t] lacks balance to perform [pay]*)
 val pay : int -> t -> t -> unit
+
+(** [pay_bank amt t] alters player [t]'s balance by an amount [amt]. For
+    example, when passing Go a player would see the equivalent of
+    [bank_transaction 200 t]. Note: [amt] may be positive or negative,
+    corresponding to receiving or paying money to the bank respectively.
+    Raises [InsufficientFunds] if the player lacks funds.*)
+val bank_transaction : int -> t -> unit
+
+(** [jail_cards t] represents the amount of get-out-of-jail free cards
+    that player [t] is currently holding. If [jail_cards t] is 0, then
+    the player will be sent to jail if prompted*)
+val jail_cards : t -> int
+
+(** [send_to_jail t] returns an identical player as [t] excdpt their
+    current square is located at "Jail", representing the action of
+    moving a piece to jail in Monopoly. *)
+val send_to_jail : t -> t
+
+(**[trade p1 p2 p1_props p2_props] is a pair of players [p1] and [p2]
+   with the various exchanged properties, mirroring if players traded
+   squares in real life. Note: cannot trade get_out_of_jail cards. *)
+val trade :
+  t -> t -> Board.square_name list -> Board.square_name list -> t * t
+
+(**[trade_cards p1 p2 amt] is a pair of players [p1] and [p2] that have
+   exchanged [amt] get-out-of-jail cards, with [p1] giving [amt] to [p2]*)
+val trade_cards : t -> t -> int -> t * t
